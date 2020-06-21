aa85778471a869f8bcd0c75008ef413fe37153c5
Sijuade Ajagunna <mahyor.sam@gmail.com>
2020-06-21 14:51:41 +0100

    add finishing touches

---
+++ README.md
@@ -0,0 +1,1 @@
+# Uduak, A simple Chatbot
--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const bodyContainer = document.querySelector('.body');
 bodyContainer.scrollTop = bodyContainer.scrollHeight;
 
 let count = 0;
-let current, jokeList, triviaList;
+let current, jokeList, triviaList, correctAns;
 
 const replies = {
   yes: [
-    'Ah! sly one, Trying out your work I see...',
+    'Trying out your work I see...',
     'Expecting an upgrade soon :(...',
-    'You lie...',
+    "You're lying...",
   ],
   no: [
     'Oh well...',
@@ -21,12 +21,14 @@ const replies = {
     'You probably lie...',
   ],
   followUp: [
-    "You're here because you're probably bored, or just checking me out <br> I'm good with jokes and trivias. Reply with your choice",
+    "I'm good with jokes and trivia. Reply with your choice",
     'Would you like a joke or trivia?',
+    'I was made for jokes and trivia. Which would you prefer?',
   ],
   joke: [
     'Get ready to be blown apart by my comedic brilliance...',
     'Coming up - a mindblowing dad joke...',
+    "I've got one!",
     'Make I burst your brain...',
   ],
   trivia: [
@@ -35,6 +37,18 @@ const replies = {
     'True or False:',
     "OK... Just answer True or False, don't try to discombobulate my basic algorithm:",
     "Alright then. <br> PS: I have been programmed to only respond to True/False answers to this, Don't make it hard please",
+  ],
+  correct: [
+    'You rock! That was right',
+    "Wow! You're almost as smart as I am",
+    "Right! You're quite decent at this",
+    'Correct',
+    "That's right. At least one of us is smart.",
+  ],
+  wrong: [
+    "That's wrong.",
+    'Wrong! Better luck next time amigo',
+    'You remind me of young me - choosing the wrong options',
   ],
   confused: [
     "I'm not as smart as you think I am, do reply accordingly...",
@@ -90,41 +104,62 @@ const getTrivia = async () => {
   return result.results;
 };
 
-// const trivia = getTrivia();
-
-// console.log(jokes);
-
 const sendJoke = (item) => {
   const { setup, punchline } = item;
 
-  sendResponse(setup, 'uduak');
-  setTimeout(() => sendResponse(punchline, 'uduak'), 400);
+  setTimeout(() => sendResponse(setup, 'uduak'), 400);
+
+  setTimeout(() => sendResponse(punchline, 'uduak'), 1000);
 };
 
 const sendTrivia = (item) => {
   const { question, correct_answer } = item;
-  console.log({ question, correct_answer });
 
+  correctAns = correct_answer;
   sendResponse(question, 'uduak');
 };
 
+const checkTriviaAnswer = (answer, correctAnswer) => {
+  let option;
+  if (answer.toLowerCase().includes('true')) {
+    option = 'True';
+  } else if (answer.toLowerCase().includes('true')) {
+    option = 'False';
+  }
+
+  return option === correctAnswer;
+};
+let game;
 const uduak = () =>
   userForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const reply = replyInput.value;
-    let response, game;
-    console.log(game);
+    let response;
 
     sendResponse(reply, 'user');
 
     if (count === 0) {
+      // Welcome
       count++;
-      if (checkStrIncludes(reply.toLowerCase(), ['yes', 'yeah', 'of course'])) {
+      if (
+        checkStrIncludes(reply.toLowerCase(), [
+          'yes',
+          'yeah',
+          'true',
+          'of course',
+        ])
+      ) {
         response = randomiseArr(replies.yes);
         sendResponse(response, 'uduak');
       } else if (
-        checkStrIncludes(reply.toLowerCase(), ['no', 'not', 'nah', 'at all'])
+        checkStrIncludes(reply.toLowerCase(), [
+          'no',
+          'not',
+          'false',
+          'nah',
+          'at all',
+        ])
       ) {
         response = randomiseArr(replies.no);
         sendResponse(response, 'uduak');
@@ -135,6 +170,7 @@ const uduak = () =>
       response = randomiseArr(replies.followUp);
       setTimeout(() => sendResponse(response, 'uduak'), 500);
     } else {
+      // Jokes
       if (checkStrIncludes(reply.toLowerCase(), ['joke', 'joke', 'laugh'])) {
         game = 'jokes';
 
@@ -144,26 +180,42 @@ const uduak = () =>
       } else if (
         checkStrIncludes(reply.toLowerCase(), ['trivia', 'triva', 'trivial'])
       ) {
+        // Trivia
         game = 'trivia';
         response = randomiseArr(replies.trivia, 'uduak');
         sendResponse(response, 'uduak');
 
         setTimeout(() => sendTrivia(randomiseArr(triviaList)), 300);
+        userForm.reset();
+        return;
+      } else if (
+        checkStrIncludes(reply.toLowerCase(), ['true', 'false']) &&
+        game === 'trivia'
+      ) {
+        // Answers to trivia
+        const score = checkTriviaAnswer(reply, correctAns);
+
+        if (score) {
+          response = randomiseArr(replies.correct);
+          setTimeout(() => sendResponse(response, 'uduak'), 500);
+        } else {
+          response = randomiseArr(replies.wrong);
+          setTimeout(() => sendResponse(response, 'uduak'), 500);
+        }
       } else {
         response = randomiseArr(replies.confused);
         sendResponse(response, 'uduak');
-        response = randomiseArr(replies.followUp);
-        setTimeout(() => sendResponse(response, 'uduak'), 300);
       }
+      setTimeout(
+        () => sendResponse(randomiseArr(replies.followUp), 'uduak'),
+        2500
+      );
     }
     userForm.reset();
-    console.log(reply);
   });
 
 window.addEventListener('load', async () => {
   jokeList = await getJokes();
   triviaList = await getTrivia();
-  console.log(jokeList);
-  console.log(triviaList);
   uduak();
 });
